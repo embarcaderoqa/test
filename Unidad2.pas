ae43415064c978aa8a931b583db36ebb127a253b
embarcaderoqa <embarcaderoqa@gmail.com>
2015-07-20 15:19:58 +0200

    Test Import

--- Unidad2.pas
+++ Unidad2.pas
@@ -8,11 +8,10 @@ uses
 
 type
   TForm1 = class(TForm)
-    procedure FormCreate(Sender: TObject);
   private
-    { Private declarations }
+    { Private-Deklarationen }
   public
-    { Public declarations }
+    { Public-Deklarationen }
   end;
 
 var
@@ -21,10 +20,5 @@ var
 implementation
 
 {$R *.fmx}
-
-procedure TForm1.FormCreate(Sender: TObject);
-begin
-ShowMessage('This is a Test');
-end;
 
 end.
